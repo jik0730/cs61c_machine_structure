bc0e08002d91243223ce1117e674482d3c3f0bc2
Ingyo Chung <jik0730s@berkeley.edu>
2016-08-05 21:07:37 -0700

    .

--- projects/04/calcDepthOptimized.c
+++ projects/04/calcDepthOptimized.c
@@ -28,23 +28,21 @@ float displacementNaive2(int dx, int dy)
 void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth, int imageHeight, int featureWidth, int featureHeight, int maximumDisplacement)
 {
     /* The two outer for loops iterate through each pixel */
-    #pragma omp parallel
-    {
-    #pragma omp parallel for shared(depth)
+    // #pragma omp parallel for shared(depth)
     for (int y = 0; y < featureHeight; y++) {
         for (int x = 0; x < imageWidth; x++) {
             depth[y * imageWidth + x] = 0;
         }
     }
 
-    #pragma omp parallel for shared(depth)
+    // #pragma omp parallel for shared(depth)
     for (int y = imageHeight - featureHeight; y < imageHeight; y++) {
         for (int x = 0; x < imageWidth; x++) {
             depth[y * imageWidth + x] = 0;
         }
     }
 
-    #pragma omp parallel for
+    // #pragma omp parallel for
     for (int y = featureHeight; y < imageHeight -featureHeight; y++)
     {
         for (int x = 0; x < imageWidth; x++)
@@ -127,6 +125,5 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
                 depth[y * imageWidth + x] = 0;
             }
         }
-    }
     }
 }
