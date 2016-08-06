4ec638af89c9b8a5ec553fc337e3a09c85705a94
Ingyo Chung <jik0730s@berkeley.edu>
2016-08-06 11:52:07 -0700

    .

--- projects/04/calcDepthOptimized.c
+++ projects/04/calcDepthOptimized.c
@@ -64,7 +64,7 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
 
 
 
-    #pragma omp parallel for collapse(2) schedule(dynamic)
+    //#pragma omp parallel for collapse(2) schedule(dynamic)
     for (int y = 0; y < imageHeight; y++)
     {
         for (int x = 0; x < imageWidth; x++)
