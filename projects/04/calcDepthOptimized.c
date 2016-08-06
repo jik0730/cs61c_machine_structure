66e8419f599bcab6edf5b08b2dcaca0fe117126b
Ingyo Chung <jik0730s@berkeley.edu>
2016-08-05 22:56:02 -0700

    .

--- projects/04/calcDepthOptimized.c
+++ projects/04/calcDepthOptimized.c
@@ -40,89 +40,98 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
     //         depth[y * imageWidth + x] = 0;
     //     }
     // }
-
-    #pragma omp parallel for
-    for (int y = 0; y < imageHeight; y++)
-    {
-        for (int x = 0; x < imageWidth; x++)
-        {   
-            /* Set the depth to 0 if looking at edge of the image where a feature box cannot fit. */
-            if ((y < featureHeight) || (y >= imageHeight - featureHeight) ||(x < featureWidth) || (x >= imageWidth - featureWidth))
-            {
-                depth[y * imageWidth + x] = 0;
-                continue;
-            }
-
-            float minimumSquaredDifference = -1;
-            int minimumDy = 0;
-            int minimumDx = 0;
-
-            /* Iterate through all feature boxes that fit inside the maximum displacement box. 
-               centered around the current pixel. */
-            for (int dy = -maximumDisplacement; dy <= maximumDisplacement; dy++)
-            {
-                for (int dx = -maximumDisplacement; dx <= maximumDisplacement; dx++)
-                {
-                    /* Skip feature boxes that dont fit in the displacement box. */
-                    if (y + dy - featureHeight < 0 || y + dy + featureHeight >= imageHeight || x + dx - featureWidth < 0 || x + dx + featureWidth >= imageWidth)
-                    {
-                        continue;
-                    }
-
-                    float squaredDifference = 0;
-
-                    /* Sum the squared difference within a box of +/- featureHeight and +/- featureWidth. */
-                    // #pragma omp reduction(+:squaredDifference)
-                    for (int boxY = -featureHeight; boxY <= featureHeight; boxY++)
-                    {
-                        int leftY = y + boxY;
-                        int rightY = y + dy + boxY;
-                        for (int boxX = -featureWidth; boxX <= featureWidth; boxX++)
-                        {
-                            int leftX = x + boxX;
-                            int rightX = x + dx + boxX;
-
-                            float difference = left[leftY * imageWidth + leftX] - right[rightY * imageWidth + rightX];
-                            squaredDifference += difference * difference;
-                        }
-                    }
-
-
-                    /* 
-                    Check if you need to update minimum square difference. 
-                    This is when either it has not been set yet, the current
-                    squared displacement is equal to the min and but the new
-                    displacement is less, or the current squared difference
-                    is less than the min square difference.
-                    */
-                    if ((minimumSquaredDifference == -1) || ((minimumSquaredDifference == squaredDifference) && (displacementNaive(dx, dy) < displacementNaive(minimumDx, minimumDy))) || (minimumSquaredDifference > squaredDifference))
-                    {
-                        minimumSquaredDifference = squaredDifference;
-                        minimumDx = dx;
-                        minimumDy = dy;
-                    }
-                }
-            }
-
-            /* 
-            Set the value in the depth map. 
-            If max displacement is equal to 0, the depth value is just 0.
-            */
-            if (minimumSquaredDifference != -1)
-            {
-                if (maximumDisplacement == 0)
-                {
-                    depth[y * imageWidth + x] = 0;
-                }
-                else
-                {
-                    depth[y * imageWidth + x] = displacementNaive2(minimumDx, minimumDy);
-                }
-            }
-            else
-            {
-                depth[y * imageWidth + x] = 0;
-            }
+    for (int y = 0; y < imageHeight; y += 4) {
+        for (int x = 0; x < imageWidth; x += 4) {
+            __m128i depth_vector = _mm_loadu_si128((__m128i *) (depth + y * imageWidth));
+            
         }
     }
+
+
+
+
+    // #pragma omp parallel for
+    // for (int y = 0; y < imageHeight; y++)
+    // {
+    //     for (int x = 0; x < imageWidth; x++)
+    //     {   
+    //         /* Set the depth to 0 if looking at edge of the image where a feature box cannot fit. */
+    //         if ((y < featureHeight) || (y >= imageHeight - featureHeight) ||(x < featureWidth) || (x >= imageWidth - featureWidth))
+    //         {
+    //             depth[y * imageWidth + x] = 0;
+    //             continue;
+    //         }
+
+    //         float minimumSquaredDifference = -1;
+    //         int minimumDy = 0;
+    //         int minimumDx = 0;
+
+    //         /* Iterate through all feature boxes that fit inside the maximum displacement box. 
+    //            centered around the current pixel. */
+    //         for (int dy = -maximumDisplacement; dy <= maximumDisplacement; dy++)
+    //         {
+    //             for (int dx = -maximumDisplacement; dx <= maximumDisplacement; dx++)
+    //             {
+    //                 /* Skip feature boxes that dont fit in the displacement box. */
+    //                 if (y + dy - featureHeight < 0 || y + dy + featureHeight >= imageHeight || x + dx - featureWidth < 0 || x + dx + featureWidth >= imageWidth)
+    //                 {
+    //                     continue;
+    //                 }
+
+    //                 float squaredDifference = 0;
+
+    //                 /* Sum the squared difference within a box of +/- featureHeight and +/- featureWidth. */
+    //                 // #pragma omp reduction(+:squaredDifference)
+    //                 for (int boxY = -featureHeight; boxY <= featureHeight; boxY++)
+    //                 {
+    //                     int leftY = y + boxY;
+    //                     int rightY = y + dy + boxY;
+    //                     for (int boxX = -featureWidth; boxX <= featureWidth; boxX++)
+    //                     {
+    //                         int leftX = x + boxX;
+    //                         int rightX = x + dx + boxX;
+
+    //                         float difference = left[leftY * imageWidth + leftX] - right[rightY * imageWidth + rightX];
+    //                         squaredDifference += difference * difference;
+    //                     }
+    //                 }
+
+
+    //                 /* 
+    //                 Check if you need to update minimum square difference. 
+    //                 This is when either it has not been set yet, the current
+    //                 squared displacement is equal to the min and but the new
+    //                 displacement is less, or the current squared difference
+    //                 is less than the min square difference.
+    //                 */
+    //                 if ((minimumSquaredDifference == -1) || ((minimumSquaredDifference == squaredDifference) && (displacementNaive(dx, dy) < displacementNaive(minimumDx, minimumDy))) || (minimumSquaredDifference > squaredDifference))
+    //                 {
+    //                     minimumSquaredDifference = squaredDifference;
+    //                     minimumDx = dx;
+    //                     minimumDy = dy;
+    //                 }
+    //             }
+    //         }
+
+    //         /* 
+    //         Set the value in the depth map. 
+    //         If max displacement is equal to 0, the depth value is just 0.
+    //         */
+    //         if (minimumSquaredDifference != -1)
+    //         {
+    //             if (maximumDisplacement == 0)
+    //             {
+    //                 depth[y * imageWidth + x] = 0;
+    //             }
+    //             else
+    //             {
+    //                 depth[y * imageWidth + x] = displacementNaive2(minimumDx, minimumDy);
+    //             }
+    //         }
+    //         else
+    //         {
+    //             depth[y * imageWidth + x] = 0;
+    //         }
+    //     }
+    // }
 }
