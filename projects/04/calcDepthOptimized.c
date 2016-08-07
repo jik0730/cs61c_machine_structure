29be98967cded093caeec2d2c6b163c841461c5f
Ingyo Chung <jik0730s@berkeley.edu>
2016-08-06 20:55:33 -0700

    .

--- projects/04/calcDepthOptimized.c
+++ projects/04/calcDepthOptimized.c
@@ -104,7 +104,7 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
     }
 
 
-    #pragma omp parallel for collapse(2) schedule(dynamic)
+    //#pragma omp parallel for collapse(2) schedule(dynamic)
     for (int y = featureHeight; y < imageHeight - featureHeight; y++)
     {
         for (int x = featureWidth; x < imageWidth - featureWidth; x++)
@@ -115,6 +115,7 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
 
             /* Iterate through all feature boxes that fit inside the maximum displacement box. 
                centered around the current pixel. */
+            #pragma omp parallel for collapse(2) schedule(dynamic)
             for (int dy = -maximumDisplacement; dy <= maximumDisplacement; dy++)
             {
                 for (int dx = -maximumDisplacement; dx <= maximumDisplacement; dx++)
