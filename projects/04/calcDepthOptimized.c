44848c54491c71f8ef927ea9658ad6c2570d375f
Ingyo Chung <jik0730s@berkeley.edu>
2016-08-07 01:07:43 -0700

    .

--- projects/04/calcDepthOptimized.c
+++ projects/04/calcDepthOptimized.c
@@ -96,7 +96,7 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
         }
     }
 
-    #pragma omp parallel for private(imageWidth, imageHeight, featureHeight, featureWidth)
+    #pragma omp parallel for private(imageWidth, imageHeight, featureHeight, featureWidth, maximumDisplacement)
     for (int y = featureHeight; y < imageHeight - featureHeight; y++)
     {
         for (int x = featureWidth; x < imageWidth - featureWidth; x++)
